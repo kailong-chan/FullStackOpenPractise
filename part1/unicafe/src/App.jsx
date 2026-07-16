f41c807a16367c313c54a68404c9f887ad24c0e4
kailong-chan <tobey98765@gmail.com>
2026-07-16 14:33:51 +0800

    done for Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,16 +5,23 @@ const App = () => {
     const [neutralCount, setNeutralCount] = useState(0)
     const [badCount, setBadCount] = useState(0)
 
+    const totalCount = goodCount + neutralCount + badCount
+    const averageCount = (goodCount - badCount) / totalCount
+    const positivePercentage = `${(goodCount / totalCount) * 100}%`
+
     return (
         <>
             <Title text="give feedback" />
-            <Button text="good" onClick={() => setGoodCount(goodCount + 1)}/>
-            <Button text="neutral" onClick={() => setNeutralCount(neutralCount + 1)}/>
-            <Button text="bad" onClick={() => setBadCount(badCount + 1)}/>
+            <Button text="good" onClick={() => setGoodCount(goodCount + 1)} />
+            <Button text="neutral" onClick={() => setNeutralCount(neutralCount + 1)} />
+            <Button text="bad" onClick={() => setBadCount(badCount + 1)} />
             <Title text="statistics" />
             <Stat text="good" count={goodCount} />
             <Stat text="neutral" count={neutralCount} />
             <Stat text="bad" count={badCount} />
+            <Stat text="all" count={totalCount} />
+            <Stat text="average" count={averageCount} />
+            <Stat text="positive" count={positivePercentage} />
         </>
     )
 }
