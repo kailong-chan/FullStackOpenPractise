ae538098838efab989056bbdf1650c05b16935b4
kailong-chan <tobey98765@gmail.com>
2026-07-19 02:36:30 +0800

    done Exercise 1.12

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,50 +1,38 @@
+import { useState } from 'react'
+
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
+    const [selected, setSelected] = useState(0)
+
+    const anecdotes = [
+        'If it hurts, do it more often.',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+        'The only way to go fast, is to go well.'
+    ]
+
+    // a helper function copied from MDN, for selecting random int between two values, exclusive maximum
+    function getRandomInt(min, max) {
+        const minCeiled = Math.ceil(min);
+        const maxFloored = Math.floor(max);
+        return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
     }
 
-    return(
-        <div>
-            <Header course={course}/>
-            <Content parts={course.parts}/>
-            <Total parts={course.parts}/>
-        </div>
-    )
-}
-
-// render the name of the course
-const Header = ({course}) => (
-    <h1>{course.name}</h1>
-)
-
-// render the parts and the number of exercise
-const Content = ({parts}) => {
     return (
         <div>
-            <p>{parts[0].name} {parts[0].exercises}</p>
-            <p>{parts[1].name} {parts[1].exercises}</p>
-            <p>{parts[2].name} {parts[2].exercises}</p>
+            <Display text={anecdotes[selected]} />
+            <Button text="next anecdote" action={() => setSelected(getRandomInt(0, anecdotes.length))} />
         </div>
     )
 }
 
-// render the total number of exercise
-const Total = ({parts}) => (
-    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-)
+const Button = ({text, action}) =>
+    <button onClick={action}>{text}</button>
+
+const Display = ({text}) =>
+    <p>{text}</p>
 
 export default App
