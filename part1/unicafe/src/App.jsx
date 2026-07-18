4cf5c7543d3e08eea885d9ffce6318a910bb1f6f
kailong-chan <tobey98765@gmail.com>
2026-07-18 18:11:49 +0800

    alternative code style for exercise 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,18 +9,6 @@ const App = () => {
     const averageCount = (goodCount - badCount) / totalCount
     const positivePercentage = `${(goodCount / totalCount) * 100}%`
 
-    if(totalCount === 0)
-        return (
-            <>
-                <Title text="give feedback" />
-                <Button text="good" onClick={() => setGoodCount(goodCount + 1)} />
-                <Button text="neutral" onClick={() => setNeutralCount(neutralCount + 1)} />
-                <Button text="bad" onClick={() => setBadCount(badCount + 1)} />
-                <Title text="statistics" />
-                <p>No feedback given</p>
-            </>
-        )
-
     return (
         <>
             <Title text="give feedback" />
@@ -28,12 +16,18 @@ const App = () => {
             <Button text="neutral" onClick={() => setNeutralCount(neutralCount + 1)} />
             <Button text="bad" onClick={() => setBadCount(badCount + 1)} />
             <Title text="statistics" />
-            <Stat text="good" count={goodCount} />
-            <Stat text="neutral" count={neutralCount} />
-            <Stat text="bad" count={badCount} />
-            <Stat text="all" count={totalCount} />
-            <Stat text="average" count={averageCount} />
-            <Stat text="positive" count={positivePercentage} />
+            {totalCount === 0 ? (
+                <p>No feedback given</p>
+            ) : (
+                <>
+                    <Stat text="good" count={goodCount} />
+                    <Stat text="neutral" count={neutralCount} />
+                    <Stat text="bad" count={badCount} />
+                    <Stat text="all" count={totalCount} />
+                    <Stat text="average" count={averageCount} />
+                    <Stat text="positive" count={positivePercentage} />
+                </>
+            )}
         </>
     )
 }
