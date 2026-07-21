dfe58c4f746b1734c8b1879f1cc1e5be235e0775
kailong-chan <tobey98765@gmail.com>
2026-07-21 15:35:49 +0800

    done exercise 1.1 and 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,19 +9,22 @@ const App = () => {
 
     return (
         <div>
-            <h1>{course}</h1>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+            <Header title={course} />
+            <Part part={part1} exercise={exercises1} />
+            <Part part={part2} exercise={exercises2} />
+            <Part part={part3} exercise={exercises3} />
+            <Total total={exercises1 + exercises2 + exercises3} />
         </div>
     )
 }
+
+const Header = ({title}) =>
+    <h1>{title}</h1>
+
+const Part = ({part, exercise}) =>
+    <p>{part}{exercise}</p>
+
+const Total = ({total}) =>
+    <p>Number of exercises {total}</p>
 
 export default App
